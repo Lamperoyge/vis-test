ec1e801f7b5fc584bd3896e59b51da9bc562282f
Adrian Mustea <mustea.adrian95@gmail.com>
2019-01-09 00:06:12 +0200

    visjs test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import { Network, Node, Edge } from '@lifeomic/react-vis-network';
 import logo from './logo.svg';
 import './App.css';
+import Demo from './demo';
+const DecoratorTest = props => {
+  return(
+    <Demo />
+  )
+};
+
+const CustomIcon = () => {
+  const viewBox = 36;
+  const iconSize = 20;
+  const pad = (viewBox - iconSize) / 2;
+  const center = viewBox / 2;
+
+  return(
+    <div><button>test</button></div>
+  )
+}
+
+class MyNetwork extends Component {
+  render() {
+    return(
+    <Network>
+    <Node id='vader' decorator={DecoratorTest} />
+    <Node id='luke' decorator={DecoratorTest} />
+    <Node id='leia' decorator={DecoratorTest} />
+    <Edge id='1' from='vader' to='luke' />
+    <Edge id='2' from='vader' to='leia' />
+    </Network>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MyNetwork />
       </div>
     );
   }
